0b23f63e240e86c2b8c4cb58f12ba564ffe65de6
Luciana Bruscino <luciana.bruscino@gmail.com>
2023-01-16 23:13:48 -0500

    amplify backend push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,11 +1,5 @@
 export type AmplifyDependentResourcesAttributes = {
     "function": {
-        "ItemHandler": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
         "FlockQuestionsAPI": {
             "Name": "string",
             "Arn": "string",
